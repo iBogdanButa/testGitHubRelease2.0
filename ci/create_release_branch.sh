7f16d7c8fd31a8fb5426a5cc4230d53273e921ea
Bogdan Buta <bogdan.buta@atos.net>
2023-02-24 15:47:59 +0200

    adding create release branch script

---
+++ ci/create_release_branch.sh
@@ -0,0 +1,110 @@
+#!/bin/bash
+
+# Description:
+### This script can run only on master branch
+
+
+# Check if the current branch name matches the pattern master
+branch_name=$(git rev-parse --abbrev-ref HEAD)
+if [[ ! $branch_name =~ ^master$ ]]; then
+  echo "Error: You should create a RC branch from the master branch."
+  exit 1
+fi
+
+# Get the release type of this new branch
+# Check if the first argument exists
+if [ -z "$1" ]; then
+    echo "Error: Release type argument missing. Possible values: major, minor, patch"
+    exit 1
+fi
+release_type="$1"
+
+if [[ "$release_type" == "major" || "$release_type" == "minor" || "$release_type" == "patch" ]]; then
+    echo "A new branch for a $release_type release will be created."
+else
+    echo "Error: Incorrect release type. Possible values: major, minor, patch"
+    exit 1
+fi
+
+# get the latest tag
+git fetch --tags
+tag=$(git tag --sort=-v:refname | grep -E '^[0-9]*+\.[0-9]*+\.[0-9]*+-SNAPSHOT$' | head -n1)
+
+# get the major, minor, and patch
+if [[ $tag =~ ([0-9]+)\.([0-9]+)\.([0-9]+)-SNAPSHOT ]]; then
+    major=${BASH_REMATCH[1]}
+    minor=${BASH_REMATCH[2]}
+    patch=${BASH_REMATCH[3]}
+else
+    echo "Error: tag ($tag) is not in the correct format" >&2
+    exit 1
+fi
+
+if [ "$release_type" == "major" ]]; then
+	((major++))
+	minor=0
+	patch=0
+elif [ "$release_type" == "minor" ]]; then
+	((minor++))
+	patch=0	
+else 
+	#patch release, will use the tag on the the master
+	#nothing to do here for now	
+fi
+
+echo "New $release_type release version base will be ${major}.${minor}.${patch}"
+
+# Setup new branch name
+branch="VERSION-${major}.${minor}.${patch}"
+
+# Setup new RC tag name for initial build
+new_rc_version="${major}.${minor}.${patch}"
+new_rc_qualifier="-RC1"
+
+# Setup RC tag name for future development builds on this release branch
+future_rc_version="${major}.${minor}.${patch}"
+future_rc_qualifier="-RC2-SNAPSHOT"
+
+# Setup new master tag
+((master_patch=patch+1))
+new_master_version="${major}.${minor}.${master_patch}"
+new_master_qualifier="-SNAPSHOT"
+
+# create RC branch
+git checkout -b "$branch"
+
+# Update the Maven version in the maven.config file
+sed -i "s/-Drevision=.*/-Drevision=$new_rc_version/" .mvn/maven.config
+sed -i "s/-Dchangelist=.*/-Dchangelist=$new_rc_qualifier/" .mvn/maven.config
+
+#drop the snapshot for now
+git config --global user.email "bogdan.buta@atos.net"
+git config --global user.name "Bogdan Buta"
+git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
+git tag "$new_rc_version$new_rc_qualifier"
+
+# Build goes here
+#
+#
+
+# Update the Maven version in the maven.config file for future RC builds
+sed -i "s/-Drevision=.*/-Drevision=$future_rc_version/" .mvn/maven.config
+sed -i "s/-Dchangelist=.*/-Dchangelist=$future_rc_qualifier/" .mvn/maven.config
+git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
+git tag "$future_rc_version$future_rc_qualifier"
+
+# push the new tags and branch to remote
+git push --set-upstream origin "$branch"
+
+# back to master branch to continue the job.
+git checkout master
+# Update the Maven version in the maven.config file
+sed -i "s/-Drevision=.*/-Drevision=$new_master_version/" .mvn/maven.config
+sed -i "s/-Dchangelist=.*/-Dchangelist=$new_master_qualifier/" .mvn/maven.config
+
+# Do the commit in master branch
+git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
+git tag "$new_master_version$new_master_qualifier"
+git push origin master
+
+
