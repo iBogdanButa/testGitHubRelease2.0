adc2a9169ddf9df36be50118b01170192319f7bf
Bogdan Buta <bogdan.buta@atos.net>
2023-02-28 11:12:36 +0200

    build_release_candidate.sh ready

--- ci/build_release_candidate.sh
+++ ci/build_release_candidate.sh
@@ -14,7 +14,68 @@ fi
 # Check if the previous tag follows the format X.Y.Z(-HFN)-RCN-SNAPSHOT
 # get the latest tag
 git fetch --tags
-tag=$(git tag --merged master --sort=-v:refname | grep -E '^[0-9]*+\.[0-9]*+\.[0-9]*+-SNAPSHOT$' | head -n1)
+tag=$(git describe --tags --abbrev=0)
 
-echo "Latest tag is $tag"
+isHF=false
+# get the major, minor, patch, RC and HF on else branch 
+if [[ $tag =~ ([0-9]+)\.([0-9]+)\.([0-9]+)-RC([0-9]+)-SNAPSHOT ]]; then
+    major=${BASH_REMATCH[1]}
+    minor=${BASH_REMATCH[2]}
+    patch=${BASH_REMATCH[3]}
+	rc=${BASH_REMATCH[4]}
+elif [[ $tag =~ ([0-9]+)\.([0-9]+)\.([0-9]+)-HF([0-9]+)-RC([0-9]+)-SNAPSHOT ]]; then
+	major=${BASH_REMATCH[1]}
+    minor=${BASH_REMATCH[2]}
+    patch=${BASH_REMATCH[3]}
+	hf=${BASH_REMATCH[4]}
+	rc=${BASH_REMATCH[5]}
+	isHF=true;
+else
+    echo "Error: tag ($tag) is not in the correct format" >&2
+    exit 1
+fi
 
+# Setup RC tag for this build
+new_rc_version="${major}.${minor}.${patch}"
+if [ "$isHF" = false ]; then
+	new_rc_qualifier="-RC$rc"
+else
+	new_rc_qualifier="-HF$hf-RC$rc"
+fi
+
+# Setup RC tag name for future development builds on this release branch
+future_rc_version="${major}.${minor}.${patch}"
+((rc++))
+if [ "$isHF" = false ]; then
+	future_rc_qualifier="-RC$rc-SNAPSHOT"
+else
+	future_rc_qualifier="-HF$hf-RC$rc-SNAPSHOT"
+fi
+echo "1. will work on branch $branch_name"
+
+# Update the Maven version in the maven.config file
+sed -i "s/-Drevision=.*/-Drevision=$new_rc_version/" .mvn/maven.config
+sed -i "s/-Dchangelist=.*/-Dchangelist=$new_rc_qualifier/" .mvn/maven.config
+echo "2. will update .mvn/maven.config on branch to $new_rc_version and $new_rc_qualifier"
+
+git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
+git tag "$new_rc_version$new_rc_qualifier" "$branch_name"
+echo "3. will commit the .mvn/maven.config changes and  create a tag $new_rc_version$new_rc_qualifier to branch: $branch_name"
+
+# Build goes here
+#
+#
+echo "4. Build goes here"
+
+# Update the Maven version in the maven.config file for future RC builds
+sed -i "s/-Drevision=.*/-Drevision=$future_rc_version/" .mvn/maven.config
+sed -i "s/-Dchangelist=.*/-Dchangelist=$future_rc_qualifier/" .mvn/maven.config
+echo "5. will update .mvn/maven.config on branch $branch_name to $future_rc_version and $future_rc_qualifier"
+
+git diff --exit-code --quiet .mvn/maven.config || git commit -m "Automatic update of version" .mvn/maven.config
+git tag "$future_rc_version$future_rc_qualifier" "$branch_name"
+echo "6. will commit the .mvn/maven.config changes and  create a tag $future_rc_version$future_rc_qualifier" 
+
+git push --tags #not recommended
+git push
+echo "7. will push the new maven version and tags to $branch_name"
